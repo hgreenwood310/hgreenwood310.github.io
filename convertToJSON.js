e6483d9b6d5227b51360a925028371e3a8f351a9
hgreenwood310 <hgreenwood18+school@chartereastdulwich.org.uk>
2025-03-10 09:40:15 +0000

    Create convertToJSON.js

---
+++ convertToJSON.js
@@ -0,0 +1,13 @@
+function convertToJSON() {
+    let form = document.getElementById("dataForm");
+    let formData = {};
+    for (let i = 0; i < form.elements.length; i++) {
+        let element = form.elements[i];
+        if (element.type !== "submit") {
+            formData[element.name] = element.value;
+        }
+    }
+    let jsonData = JSON.stringify(formData);
+    let jsonOutput = document.getElementById("jsonOutput");
+    jsonOutput.innerHTML = "<pre>" + jsonData + "</pre>";
+}
